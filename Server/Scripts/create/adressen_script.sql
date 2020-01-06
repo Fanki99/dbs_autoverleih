55663ff4d8a8dea61f9d76bc1ac04d4a3e6776f2
fanki99 <thomas.fankhauser1@gmail.com>
2020-01-06 16:36:51 +0100

    add script for creating all tables
    Signed-off-by: fanki99 <thomas.fankhauser1@gmail.com>

--- Server/Scripts/create/adressen_script.sql
+++ Server/Scripts/create/adressen_script.sql
@@ -12,12 +12,12 @@ DELETE T_ADRESSEN;
 
 -- insert
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (1,'Bernardgasse', '333', '1070', 'Wien');
-INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (2,'Thaliastraﬂe', '333', '1160', 'Wien');
+INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (2,'Thaliastra√üe', '333', '1160', 'Wien');
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (3,'Hochstaedtplatz', '333', '1200', 'Wien');
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (4,'Einoedhofweg', '333', '8042', 'Graz');
-INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (5,'Salzachstraﬂe', '333', '1200', 'Wien');
-INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (6,'Adalbert-Stifter-Straﬂe', '333', '1200', 'Wien');
-INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (7,'Mariahilferstraﬂe', '333', '1060', 'Wien');
+INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (5,'Salzachstra√üe', '333', '1200', 'Wien');
+INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (6,'Adalbert-Stifter-Stra√üe', '333', '1200', 'Wien');
+INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (7,'Mariahilferstra√üe', '333', '1060', 'Wien');
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (8,'Lerchengasse', '333', '1010', 'Wien');
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (9,'Neubaugasse', '333', '1020', 'Wien');
 INSERT INTO T_ADRESSEN (id, strasse, hausnummer, plz, ort) VALUES (10,'Almdudlergasse', '333', '1040', 'Wien');
